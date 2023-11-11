3c9227fd84586c1ad8cb83f50f6fcd56c722154e
jaimon <jaimon.abraham87@gmail.com>
2023-11-11 01:24:09 -0500

    Update performance_benchmark_real_estate_sqlite.py

--- Decision_analytics/Module8/SQLite/performance_benchmark_real_estate_sqlite.py
+++ Decision_analytics/Module8/SQLite/performance_benchmark_real_estate_sqlite.py
@@ -6,7 +6,7 @@ import time
 db_file_path = '/Users/Jai/Documents/Git_remote/Decision_analytics/Module8/SQLite/performance_benchmark_real_estate_sales_sqlite.db'
 
 # Read the CSV file into a pandas DataFrame
-df = pd.read_csv('/Users/Jai/Documents/Git_remote/Decision_analytics/Module8/Data/Real_Estate_Sales_2001-2020_GL.csv')
+df = pd.read_csv('/Users/Jai/Desktop/Data/Real_Estate_Sales_2001-2020_GL.csv')
 
 # Establish a connection to the database and create the table
 conn = sqlite3.connect(db_file_path)
@@ -39,6 +39,7 @@ try:
             start_time = time.perf_counter()
 
             cur.execute(query)
+            cur.fetchall()
             end_time = time.perf_counter()
 
             run_times.append(end_time - start_time)
